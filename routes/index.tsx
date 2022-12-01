fe190ab089946036cd765071f0456a0bd915236b
mantequilla <llacuagrayce@gmail.com>
2022-11-30 19:28:58 -0500

    first commit

---
+++ routes/index.tsx
@@ -0,0 +1,12 @@
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  return (
+    <div class="p-4 mx-auto max-w-screen-md">
+      <head>
+        <link rel="stylesheet" href="./app.css" />
+      </head>
+      <h1>THIS IS NEW PAGE</h1>
+    </div>
+  );
+}
